4da7cf33379789658d8b72b4f40a5b868e082b8b
Odili <odiliejeh09@gmail.com>
2026-03-18 13:17:04 +0100

    "tasks"

--- src/com/realEstate/controllers/GateManControllers.java
+++ src/com/realEstate/controllers/GateManControllers.java
@@ -1,7 +1,11 @@
 package com.realEstate.controllers;
 
 import com.realEstate.dtos.requests.ValidateCodeRequest;
+import com.realEstate.dtos.responses.ValidateCodeResponse;
+import com.realEstate.exceptions.GatePassDoesNotExistException;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import com.realEstate.services.GatePassAccessService;
@@ -13,7 +17,13 @@ public class GateManControllers {
     private GatePassAccessService services;
 
     public ResponseEntity<?> validateGatePass(ValidateCodeRequest validateCodeRequest){
-        return null;
+        try {
+            ValidateCodeResponse validateCodeResponse = new ValidateCodeResponse();
+            return ResponseEntity.status(HttpStatus.FOUND).body(validateCodeResponse);
+        }
+        catch(GatePassDoesNotExistException error){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error.getMessage());
+        }
     }
 
 }
--- src/com/realEstate/controllers/ResidentControllers.java
+++ src/com/realEstate/controllers/ResidentControllers.java
@@ -7,9 +7,9 @@ import com.realEstate.services.GatePassAccessService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 
 @RestController
